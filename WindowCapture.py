c4fd9f8e53d0502f55f2472be4c943a70e9b8611
TomaszKarolewski <tomasz.karolewski7@gmail.com>
2022-12-16 00:42:05 +0100

    fix: stretched image added: FPS preview on image

--- WindowCapture.py
+++ WindowCapture.py
@@ -4,6 +4,7 @@ import win32con
 import win32gui
 import win32ui
 from time import time
+import ctypes
 
 """
 The creator of the class I was based on is https://learncodebygaming.com/blog/fast-window-capture
@@ -11,16 +12,6 @@ The creator of the class I was based on is https://learncodebygaming.com/blog/fa
 
 
 class WindowCapture:
-
-    # properties
-    w = 0
-    h = 0
-    hwnd = None
-    cropped_x = 0
-    cropped_y = 0
-    offset_x = 0
-    offset_y = 0
-
     # constructor
     def __init__(self, window_name):
         # find the handle for the window we want to capture
@@ -29,13 +20,16 @@ class WindowCapture:
             raise Exception('Window not found: {}'.format(window_name))
 
         # get the window size
+        user32 = ctypes.windll.user32
+        user32.SetProcessDPIAware()
+
         window_rect = win32gui.GetWindowRect(self.hwnd)
         self.w = window_rect[2] - window_rect[0]
         self.h = window_rect[3] - window_rect[1]
 
         # account for the window border and titlebar and cut them off
-        border_pixels = 8
-        titlebar_pixels = 30
+        border_pixels = 9
+        titlebar_pixels = 37
         self.w = self.w - (border_pixels * 2)
         self.h = self.h - titlebar_pixels - border_pixels
         self.cropped_x = border_pixels
@@ -47,7 +41,6 @@ class WindowCapture:
         self.offset_y = window_rect[1] + self.cropped_y
 
     def get_screenshot(self):
-
         # get the window image data
         wDC = win32gui.GetWindowDC(self.hwnd)
         dcObj = win32ui.CreateDCFromHandle(wDC)
@@ -58,7 +51,10 @@ class WindowCapture:
         cDC.BitBlt((0, 0), (self.w, self.h), dcObj, (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
 
         # convert the raw data into a format opencv can read
-        #dataBitMap.SaveBitmapFile(cDC, 'debug.bmp')
+        # flag = 1
+        # if flag == 1:
+        #     dataBitMap.SaveBitmapFile(cDC, 'debug.bmp')
+        #     flag = 0
         signedIntsArray = dataBitMap.GetBitmapBits(True)
         img = np.fromstring(signedIntsArray, dtype='uint8')
         img.shape = (self.h, self.w, 4)
@@ -72,7 +68,7 @@ class WindowCapture:
         # drop the alpha channel, or cv.matchTemplate() will throw an error like:
         #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type()
         #   && _img.dims() <= 2 in function 'cv::matchTemplate'
-        img = img[...,:3]
+        img = img[..., :3]
 
         # make image C_CONTIGUOUS to avoid errors that look like:
         #   File ... in draw_rectangles
@@ -87,15 +83,19 @@ class WindowCapture:
 if __name__ == '__main__':
     wincap = WindowCapture('DSCH_TRAIN_dataset.7z (wersja testowa)')
     loop_time = time()
-    while (True):
-
+    while True:
         # get an updated image of the game
         screenshot = wincap.get_screenshot()
 
-        cv.imshow('Computer Vision', screenshot)
-
         # debug the loop rate
-        print('FPS {}'.format(1 / (time() - loop_time)))
+        cv.putText(img=screenshot, text='FPS {:.0f}'.format(1 / (time() - loop_time)),
+                   org=(100, 100),
+                   fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                   fontScale=1,
+                   color=(0, 0, 0),
+                   thickness=2,
+                   lineType=2)
+        cv.imshow('Computer Vision', screenshot)
         loop_time = time()
 
         # press 'q' with the output window focused to exit.
